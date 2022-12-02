4b6091e5978efad3d2f43a874dffa41ef9143683
MingJie Li <kadsg0769@163.com>
2022-12-02 22:37:36 +0800

    完善service

--- src/main/java/service/EventPlayerService.java
+++ src/main/java/service/EventPlayerService.java
@@ -13,13 +13,22 @@ import java.util.List;
 
 public class EventPlayerService {
     private SqlSession sqlSession;
+
+    public void insert(EventPlayer eventPlayer) throws IOException {
+        sqlSession = DBUtil.getSqlSession();
+        EventPlayerMapper mapper = sqlSession.getMapper(EventPlayerMapper.class);
+        mapper.insert(eventPlayer);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
     /**
      * 获取运动员的报名列表
      */
     public List<EventPlayer> queryAllEventOfPlayer(User user) throws IOException {
         sqlSession = DBUtil.getSqlSession();
         EventPlayerMapper mapper = sqlSession.getMapper(EventPlayerMapper.class);
-        List<EventPlayer> itemList = (List<EventPlayer>) mapper.queryAllByIdSport(user.getAccount());
+        List<EventPlayer> itemList = (List<EventPlayer>) mapper.queryAllByIdPlayer(user.getAccount());
         sqlSession.commit();
         sqlSession.close();
         return itemList;
