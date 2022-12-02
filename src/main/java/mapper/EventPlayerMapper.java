de66ef0436310a88f169f88c76f126541a4ee991
MingJie Li <kadsg0769@163.com>
2022-12-02 22:37:08 +0800

    修正错误的返回值

--- src/main/java/mapper/EventPlayerMapper.java
+++ src/main/java/mapper/EventPlayerMapper.java
@@ -4,13 +4,15 @@ import bean.EventPlayer;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 public interface EventPlayerMapper {
-    @Insert("insert into event_player values (#{id_sport},#{id_item},#{grade},#{join})")
+    @Insert("insert into event_player values (#{id_player},#{id_item},#{grade},#{join})")
     void insert(EventPlayer eventPlayer);
 
     /**
      * 获得运动员的参赛信息
      */
-    @Select("select * from event_player where id_sport=#{id_sport}")
-    EventPlayer queryAllByIdSport(String id_sport);
+    @Select("select * from event_player where id_player=#{id_player}")
+    List<EventPlayer> queryAllByIdPlayer(String id_player);
 }
