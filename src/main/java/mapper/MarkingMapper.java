a24fc36cbd6b8d770c194f746aac25db9ef7218a
MingJie Li <kadsg0769@163.com>
2022-12-03 13:25:36 +0800

    完善功能

--- src/main/java/mapper/MarkingMapper.java
+++ src/main/java/mapper/MarkingMapper.java
@@ -2,8 +2,13 @@ package mapper;
 
 import bean.Marking;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface MarkingMapper {
     @Insert("insert into marking values (#{id_referee},#{id_event},#{id_player},#{grade})")
     void insert(Marking marking);
+
+    @Select("select * form marking where id_item=#{id_item} and id_player=#{id_player}}")
+    Marking search(@Param("id_item") String id_item, @Param("id_player") String id_player);
 }
--- src/main/java/service/MarkingService.java
+++ src/main/java/service/MarkingService.java
@@ -25,4 +25,18 @@ public class MarkingService {
         sqlSession.commit();
         sqlSession.close();
     }
+
+    /**
+     * 获取单条打分记录
+     * @param id_item 项目id
+     * @param id_player 运动员id
+     */
+    public Marking search(String id_item, String id_player) throws IOException {
+        sqlSession = DBUtil.getSqlSession();
+        MarkingMapper mapper = sqlSession.getMapper(MarkingMapper.class);
+        Marking marking = mapper.search(id_item, id_player);
+        sqlSession.commit();
+        sqlSession.close();
+        return marking;
+    }
 }
