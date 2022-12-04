2ddc0dab850477c893aa80ec30748f5530df618b
MingJie Li <kadsg0769@163.com>
2022-12-04 16:00:16 +0800

    编写service

---
+++ src/main/java/service/EventService.java
@@ -0,0 +1,50 @@
+package service;
+
+import bean.Marking;
+import bean.Score;
+import bean.item.Item;
+import bean.user.Player;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EventService {
+    /**
+     * 结束赛事
+     * @param id_item 赛事编号
+     */
+    public void announceItem(String id_item) throws IOException {
+        // 结束赛事
+        ItemService itemService = new ItemService();
+        Item item = itemService.queryItemById(id_item);
+        item.setIs_over(true);
+        new ItemService().update(item);
+
+        // 获取参赛选手
+        List<Player> playerList = new EventPlayerService().queryAllPlayerOfItem(item);
+        // 获取选手的打分表并统计
+        if (playerList != null) {
+            List<Score> scoreList = new ArrayList<>();
+            for (Player player : playerList) {
+                List<Marking> markingList = new MarkingService().queryByIdItemAndIdPlayer(id_item, player.getAccount());
+                // 统计
+                int count = markingList.size();
+                double sum = 0 , grade;
+                for (Marking marking : markingList)
+                    sum += marking.getGrade();
+                // 得分
+                grade = sum / count;
+                Score score = new Score(id_item, player.getAccount(), grade);
+                scoreList.add(score);
+            }
+            // 对scoreList进行排序得出排名
+            Collections.sort(scoreList);
+            for (int i = 0; i < scoreList.size(); ++i)
+                scoreList.get(i).setRank(i + 1);
+            // 写入到数据库
+            new ScoreService().insert(scoreList);
+        }
+    }
+}
---
+++ src/main/java/service/ScoreService.java
@@ -0,0 +1,36 @@
+package service;
+
+import bean.Score;
+import mapper.ScoreMapper;
+import org.apache.ibatis.session.SqlSession;
+import util.DBUtil;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ScoreService {
+    private SqlSession sqlSession;
+    // 将成绩单保存到到数据库中
+    public void insert(List<Score> scoreList) throws IOException {
+        sqlSession = DBUtil.getSqlSession();
+        ScoreMapper mapper = sqlSession.getMapper(ScoreMapper.class);
+        for (Score score : scoreList)
+            mapper.insert(score);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    /**
+     * 获取赛事的成绩单
+     * @param id_item 赛事编号
+     * @return
+     */
+    public List<Score> queryScoreListOfItem(String id_item) throws IOException {
+        sqlSession = DBUtil.getSqlSession();
+        ScoreMapper mapper = sqlSession.getMapper(ScoreMapper.class);
+        List<Score> scoreList = mapper.queryAllByIdItem(id_item);
+        sqlSession.commit();
+        sqlSession.close();
+        return scoreList;
+    }
+}
